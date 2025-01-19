e851b2dbec04d53b2eb4ed5579a8019007e42a89
Philly Chien <chienaeae>
2025-01-19 01:16:41 -0500

    feat(web): add i18n feature

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,21 +1,28 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage/index.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 import Navbar from './components/Navbar.tsx';
 
+// import i18n (needs to be bundled
 import './index.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Navbar />}>
-                    <Route index element={<HomePage />} />
-                </Route>
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<Navbar />}>
+                            <Route index element={<HomePage />} />
+                        </Route>
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </Suspense>
+        </I18nextProvider>
     </StrictMode>
 );
--- apps/web/src/pages/HomePage/index.tsx
+++ apps/web/src/pages/HomePage/index.tsx
@@ -1,3 +1,24 @@
+import { useTranslation } from 'react-i18next';
+
 export default function HomePage() {
-    return <div className="text-3xl font-bold underline">HomePage</div>;
+    const { t, i18n } = useTranslation();
+    return (
+        <div className="text-3xl font-bold">
+            <div className="flex gap-2">
+                <button
+                    className="bg-blue-500 text-white p-2 rounded-md"
+                    onClick={() => i18n.changeLanguage('fr')}
+                >
+                    fr
+                </button>
+                <button
+                    className="bg-blue-500 text-white p-2 rounded-md"
+                    onClick={() => i18n.changeLanguage('en')}
+                >
+                    en
+                </button>
+            </div>
+            {t('title')}
+        </div>
+    );
 }
