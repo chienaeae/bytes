4eb879c71aaa7dde25275e9816c8369a111fe59e
ting850911 <ytwang4@myseneca.ca>
2025-03-06 20:25:46 -0500

    feat: fix page position to (0, 0) after redirect to new page

--- apps/web/src/components/layout/HomeLayout.tsx
+++ apps/web/src/components/layout/HomeLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 
 import Footer from '@/components/footer/Footer';
 import { Header } from '@/components/header/Header';
 
 export function HomeLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="w-screen flex flex-col">
       <Header />
