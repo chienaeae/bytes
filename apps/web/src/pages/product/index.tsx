be0dff1676ecce137bbd59271e2e3f2a8ec66315
Philly Chien <chienaeae>
2025-03-13 00:51:40 -0400

    feat(web): use useParams to avoid manual parse id

--- apps/web/src/pages/product/index.tsx
+++ apps/web/src/pages/product/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+import { useParams } from 'react-router';
 
 import { Product } from '@/model/product';
 import { getProducts } from '@/service/core/products';
@@ -10,8 +10,7 @@ import RecommendSection from './RecommendSection';
 
 export default function ProductPage() {
   const [product, setProduct] = useState<Product | null>(null);
-  const location = useLocation();
-  const id = location.pathname.split('/').pop();
+  const { id } = useParams();
   useEffect(() => {
     const fetchData = async () => {
       if (!id) {
